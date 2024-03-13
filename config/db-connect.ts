6f6360eeb6d94c5c8340f29b4b356dc3c7d7a1c2
sporium <bugonort1@gmail.com>
2024-03-13 16:41:52 +0200

    local mongo

--- config/db-connect.ts
+++ config/db-connect.ts
@@ -1,6 +1,6 @@
 import mongoose, { type Mongoose } from 'mongoose'
 
-const connectDB = async (url: string = 'mongodb://mongo:27017/node_DB'): Promise<Mongoose> => {
+const connectDB = async (url: string = 'mongodb://mongo:27017/DB'): Promise<Mongoose> => {
   const options: mongoose.ConnectOptions = {
     autoIndex: true
   }
