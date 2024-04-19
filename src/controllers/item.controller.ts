993693979b59fe9ea2f051151205cac7ecaf42b6
sporium <bugonort1@gmail.com>
2024-04-19 15:22:35 +0300

    upload image to AWS

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -17,20 +17,12 @@ const create = async (
   try {
     const decoded = decodeJwt(req?.headers.authorization)
     const user = await User.findById(decoded.id)
-    let image: IImage | null = null
-    const uploadedFile = await uploadToAWS(req.files?.image)
-
-    if (uploadedFile && Object.prototype.hasOwnProperty.call(uploadedFile, 'Location')) {
-      image = {
-        name: req.files?.image.name ?? '',
-        src: (uploadedFile as AWSData).Location
-      }
-    }
+    const formedImages = await formImageData(req)
     const item = await Item.create({
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
-      images: image ? [image] : [],
+      images: formedImages,
       user: user?._id
     })
     const token = decodeJwt(req.headers.authorization)
@@ -50,26 +42,18 @@ export interface UpdateItemParams extends IItem {
 }
 
 const update = async (
-  req: ReqWithFiles<UpdateItemParams, Record<string, unknown>, Record<string, unknown>, Record<string, unknown>, 'image'>,
+  req: ReqWithFiles<UpdateItemParams, Record<string, unknown>, Record<string, unknown>, Record<string, unknown>, `image[${number}]`>,
   res: Response<IItemResource | IErrorResponse>): Promise<void> => {
   const itemID = req.params.id
   try {
     const decoded = decodeJwt(req?.headers.authorization)
     const user = await User.findById(decoded.id)
     const item = await Item.findById(itemID)
-    let image: IImage | null = null
-    const uploadedFile = await uploadToAWS(req.files?.image)
-
-    if (uploadedFile && Object.prototype.hasOwnProperty.call(uploadedFile, 'Location')) {
-      image = {
-        name: req.files?.image.name ?? '',
-        src: (uploadedFile as AWSData).Location
-      }
-    }
+    const formedImages = await formImageData(req)
     if (decoded.id === item?.user?._id.toString()) {
       item?.set({
         ...req.body,
-        images: [...(Array.isArray(item.images) ? item.images : []), image],
+        images: [...(Array.isArray(item.images) ? item.images : []), ...formedImages],
         user
       })
       await item?.save()
@@ -132,7 +116,7 @@ export interface AWSData {
   Key: string
   Bucket: string
 }
-const uploadToAWS = async (image: UploadedFile | undefined): Promise<AWSData | null | Error> => {
+const uploadToAWS = async (image: UploadedFile | undefined): Promise<AWSData | null> => {
   if (image) {
     const uploadParams = {
       Bucket: AWS_BUCKET_NAME,
@@ -141,19 +125,33 @@ const uploadToAWS = async (image: UploadedFile | undefined): Promise<AWSData | n
       ContentType: image.mimetype,
       ACL: 'public-read'
     }
-    return await new Promise((resolve, reject) => {
-      s3.upload(uploadParams, function (err: Error, data: AWSData) {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
+    return s3.upload(uploadParams).promise()
   }
   return null
 }
 
+const formImageData = async (
+  req: ReqWithFiles<UpdateItemParams, Record<string, unknown>, Record<string, unknown>, Record<string, unknown>, `image[${number}]`>
+): Promise<IImage[]> => {
+  let uploadedFiles: AWSData[] | null = null
+  if (req?.files) {
+    const promises: Array<Promise<AWSData | null>> = []
+    Object.keys(req?.files).forEach((key, index) => {
+      promises.push(uploadToAWS(req?.files?.[`image[${index}]`]))
+    })
+    await Promise.all(promises).then(function (data: Array<AWSData | null>) {
+      uploadedFiles = data.filter(Boolean) as AWSData[]
+    }).catch(function (err: Error) {
+      console.log(err)
+    })
+  }
+  return (uploadedFiles ?? [])?.map((el: AWSData) => {
+    return {
+      name: el.key.replace(FILES_STORAGE, ''),
+      src: el.Location
+    }
+  })
+}
 const getItemsByUser = async (req: ApiRequestInterface, res: Response<IItemResource[] | IErrorResponse>): Promise<void> => {
   try {
     const decoded = decodeJwt(req?.headers.authorization)
